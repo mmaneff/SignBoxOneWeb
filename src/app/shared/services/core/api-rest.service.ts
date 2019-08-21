cf486acb81b5523b97052d2d46c8fafe1bc7eca4
Mateo Maneff <mmaneff@gmail.com>
2019-08-20 23:52:18 -0300

    maquetación y mejoras servicio

--- src/app/shared/services/core/api-rest.service.ts
+++ src/app/shared/services/core/api-rest.service.ts
@@ -1,6 +1,6 @@
-import { Observable, BehaviorSubject, Subject, throwError } from 'rxjs';
+import { Observable, BehaviorSubject, Subject, throwError, of } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { map, catchError, switchMap, mergeMap } from 'rxjs/operators';
+import { map, catchError, switchMap, mergeMap, tap } from 'rxjs/operators';
 import {
   HttpClient,
   HttpResponse,
@@ -9,6 +9,14 @@ import {
 } from '@angular/common/http';
 import { ModelMapper } from '../../../core/mapping/modelMapper';
 
+
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Access-Control-Allow-Origin': '*',
+    'Content-Type': 'application/json'
+  })
+};
 
 
 @Injectable({
@@ -32,6 +40,71 @@ export class ApiRestService {
           return new ModelMapper(itemType).map(data);
         })
     );
+  }
+
+
+  public getById<T>(url: string, endPoint: string, id: number): Observable<T> {
+    if (!url || !endPoint) {
+      return;
+    }
+
+    return this.http.get<T>(`${url}${endPoint}${id}`)
+      .pipe(
+        map((data) => {
+          // return new ModelMapper.map(data);
+          return data;
+        })
+    );
+  }
+
+  public create<T>(url: string, endPoint: string, item: any): Observable<T> {
+    if (!url || !endPoint) {
+      return;
+    }
+
+    return this.http.post<T>(`${url}${endPoint}`, JSON.stringify(item), httpOptions)
+      .pipe(
+        tap(_ => console.log(`create`)),
+        catchError(this.handleError<T>('create'))
+    );
+  }
+
+  public update<T>(url: string, endPoint: string, item: any): Observable<T> {
+    if (!url || !endPoint) {
+      return;
+    }
+
+    return this.http.put<T>(`${url}${endPoint}${item.id}`, JSON.stringify(item), httpOptions)
+      .pipe(
+        tap(_ => console.log(`update`)),
+        catchError(this.handleError<any>('update'))
+    );
+  }
+
+  public delete<T>(url: string, endPoint: string, id: number): Observable<T> {
+    if (!url || !endPoint) {
+      return;
+    }
+
+    return this.http.delete<T>(`${url}${endPoint}${id}`, httpOptions)
+      .pipe(
+        tap(_ => console.log(`deleted product id=${id}`)),
+        catchError(this.handleError<T>('delete'))
+      );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
 }
